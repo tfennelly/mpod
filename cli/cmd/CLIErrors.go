8c1fcca5b74951bd10c2bd9fe2381c792aefbb53
Tom Fennelly <tom.fennelly@gmail.com>
2024-10-23 21:14:37 +0100

    Cleanup CLIErr a bit - prob not needed though

--- cli/cmd/CLIErrors.go
+++ cli/cmd/CLIErrors.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"strings"
+	"fmt"
 )
 
 type CLIErr struct {
@@ -11,33 +12,20 @@ type CLIErr struct {
 
 // An example of an argument is 'mpod start', where start is the argument
 func InvalidArg(command string, invalidArg string) CLIErr {
-	var messageSB strings.Builder
-	messageSB.WriteString("InvalidArgumentError: ")
-	messageSB.WriteString("'" + invalidArg + "'")
-	messageSB.WriteString(" is not a valid argument for command: ")
-	messageSB.WriteString(command)
-
-	InvalidArgumentError := CLIErr{
-		name:    "InvalidArgumentError",
-		message: messageSB.String(),
-	}
-
-	return InvalidArgumentError
+	return newCLIErr("InvalidArgumentError", command, invalidArg)
 }
 
 // A flag can be thought of as 'an argument to an argument'
 // e.g 'mpod start --name "my-container"' where '--name' is the flag
 func InvalidFlag(argument string, invalidFlag string) CLIErr {
-	var messageSB strings.Builder
-	messageSB.WriteString("InvalidFlagError: ")
-	messageSB.WriteString("'" + invalidFlag + "'")
-	messageSB.WriteString(" is not a valid flag for argument: ")
-	messageSB.WriteString(argument)
+	return newCLIErr("InvalidFlagError", command, invalidArg)
+}
 
-	InvalidFlagError := CLIErr{
+func newCLIErr(errorType string, argument string, invalidFlag string) CLIErr {
+	invalidFlagError := CLIErr{
 		name:    "InvalidFlagError",
-		message: messageSB.String(),
+		message: fmt.Sprintf("%s: '%s' is not a valid flag for argument: %s", errorType, argument, invalidFlag),
 	}
 
-	return InvalidFlagError
+	return invalidFlagError
 }

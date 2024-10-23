bade32ae727117ee7950f606355de4cb106104a2
Tom Fennelly <tom.fennelly@gmail.com>
2024-10-23 22:17:49 +0100

    little refactors for the Mike

--- cli/cmd/handleArgs.go
+++ cli/cmd/handleArgs.go
@@ -6,15 +6,12 @@ import (
 	"os"
 )
 
-// Defines the *structure* of a valid command in application logic
-// NOTE that the flags - such as name are arbitrary, and validity is only dependent on position thus they are omitted from this struct
-type command struct {
-	use          string
-	argsAndFlags []string // any arbitrary args - this struct doesn't verify validity of args
-	short        string
-	long         string
-	run          func()
-}
+
+// ************************************************************************
+// I don't think any of this is needed of you use the flags library
+// ************************************************************************
+
+
 
 func main() {
 	if len(os.Args) == 1 {
@@ -24,49 +21,39 @@ func main() {
 	// detect what the command was
 	// if the command is a valid command, pass it to the relevant handler function for that command
 	// if the command is invalid, raise an error based on the type of misuse
-	command, _ := createCommand(os.Args)
-	command.run()
+	command, err := createCommand(os.Args)
+	if err != nil {
+		panic(err)
+	}
+	command()
 }
 
 func isValidIndex(slice []string, index int) bool {
 	return index >= 0 && index < len(slice)
 }
 
-func createCommand(programArgs []string) (command, error) {
+func createCommand(programArgs []string) (func(), error) {
 	switch programArgs[1] {
 	case "start":
+		let name = "default"
 		// start has non-mandatory flag of '--name'
 		if isValidIndex(programArgs, 2) {
 			if programArgs[2] != "--name" {
 
 			}
 		}
-		// check for validity of start command logic
-		thisStartCommand := command{
-			use:          programArgs[0],
-			argsAndFlags: []string{},
-			short:        "scrapShort",
-			long:         "scrapLong",
-			run: func() {
-				fmt.Println("scrapPrint")
-			},
-		}
-		return thisStartCommand, nil
+		return func() {
+			fmt.Println("Starting the %q thing ..", name)
+			// add code to start ...
+		}, nil
 	case "stop":
-		// check for validity of stop command logic
-		thisStopCommand := command{
-			use:          programArgs[0],
-			argsAndFlags: []string{},
-			short:        "scrapShort",
-			long:         "scrapLong",
-			run: func() {
-				fmt.Println("scrapPrint")
-			},
-		}
-		return thisStopCommand, nil
+		return func() {
+			fmt.Println("Stopping the thing ..")
+			// add code to stop ...
+		}, nil
 	default:
 		err := errors.New("this is the default err")
 
-		return command{}, err
+		return nil, err
 	}
 }

dd8cf62467f6fd676b1675c6675e90c1e7b66009
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-10-23 18:50:46 +0100

    handleArgs handling name flag

--- cli/cmd/handleArgs.go
+++ cli/cmd/handleArgs.go
@@ -6,17 +6,14 @@ import (
 	"os"
 )
 
-// Defines the structure of a valid command
-// this will have to be checked to see if the args are valid for the command
-// NOTE that the flags - such as name are arbitrary, and validity is only
-// dependent on position thus they are omitted from this struct
-// 		e.g. the name of container to start must be start.index + 1
+// Defines the *structure* of a valid command in application logic
+// NOTE that the flags - such as name are arbitrary, and validity is only dependent on position thus they are omitted from this struct
 type command struct {
-	use   string
-	args  []string // any arbitrary args - this struct doesn't verify validity of args
-	short string
-	long  string
-	run   func()
+	use          string
+	argsAndFlags []string // any arbitrary args - this struct doesn't verify validity of args
+	short        string
+	long         string
+	run          func()
 }
 
 func main() {
@@ -27,19 +24,29 @@ func main() {
 	// detect what the command was
 	// if the command is a valid command, pass it to the relevant handler function for that command
 	// if the command is invalid, raise an error based on the type of misuse
-	createCommand(os.Args)
+	command, _ := createCommand(os.Args)
+	command.run()
+}
+
+func isValidIndex(slice []string, index int) bool {
+	return index >= 0 && index < len(slice)
 }
 
 func createCommand(programArgs []string) (command, error) {
 	switch programArgs[1] {
 	case "start":
-		fmt.Println(programArgs[2])
+		// start has non-mandatory flag of '--name'
+		if isValidIndex(programArgs, 2) {
+			if programArgs[2] != "--name" {
+
+			}
+		}
 		// check for validity of start command logic
 		thisStartCommand := command{
-			use:   programArgs[0],
-			args:  []string{},
-			short: "scrapShort",
-			long:  "scrapLong",
+			use:          programArgs[0],
+			argsAndFlags: []string{},
+			short:        "scrapShort",
+			long:         "scrapLong",
 			run: func() {
 				fmt.Println("scrapPrint")
 			},
@@ -48,10 +55,10 @@ func createCommand(programArgs []string) (command, error) {
 	case "stop":
 		// check for validity of stop command logic
 		thisStopCommand := command{
-			use:   programArgs[0],
-			args:  []string{},
-			short: "scrapShort",
-			long:  "scrapLong",
+			use:          programArgs[0],
+			argsAndFlags: []string{},
+			short:        "scrapShort",
+			long:         "scrapLong",
 			run: func() {
 				fmt.Println("scrapPrint")
 			},
